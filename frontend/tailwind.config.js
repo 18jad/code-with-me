23b75e784adff637027b9508dbf0889ab00cc6a5
Jad Yahya <jadoyahya@gmail.com>
2022-10-21 00:57:46 +0300

    [Frontend | Config]: Added tailwindcss config file

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "src", "./src"],
+  theme: {
+    colors: {},
+    fontFamily: {},
+    extend: {},
+  },
+  plugins: [],
+};
