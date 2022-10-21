1514546bd12df338646cc898c674f472008e3ddc
Jad Yahya <jadoyahya@gmail.com>
2022-10-21 23:12:09 +0300

    [Frontend | Views]: Added hero section to homepage

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -5,6 +5,31 @@ const Home = () => {
   return (
     <div className={styles.mainContainer}>
       <Navbar />
+      <div className={styles.heroSection}>
+        <div className={styles.introduction}>
+          <h1>A collaborative online IDE.</h1>
+          <p>
+            Code online with you friends, increase your productivity and chat
+            together. <br />
+            Save your projects online and don't worry about them!
+          </p>
+        </div>
+        <div className={styles.interactions}>
+          <button className={styles.getStartedBtn}>Get Started</button>
+          <div className={styles.mobileDownload}>
+            <p>
+              Mobile app available for both <br />
+              <span className={styles.platformSelection}>
+                <b>Android</b>
+              </span>
+              and
+              <span className={styles.platformSelection}>
+                <b>IOS</b>
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
