0f3f1fcc823e69b5a2c2316af1e100174ed0b6e8
Jad Yahya <jadoyahya@gmail.com>
2022-10-22 01:42:13 +0300

    [Frontend | Config]: Added stars particles configuration

---
+++ frontend/src/views/particles/StarsParticles.jsx
@@ -0,0 +1,51 @@
+import { loadStarsPreset } from "tsparticles-preset-stars";
+
+// Init particles engine
+const initEngine = async (engine) => {
+  console.log(engine);
+  await loadStarsPreset(engine);
+};
+
+// Particles engine options
+const starsOptions = {
+  preset: "stars",
+  fullScreen: { enable: false },
+  background: {
+    color: {
+      value: "invinsible",
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff", // stars color
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      directions: "none",
+      enable: true,
+      random: false,
+      speed: 0.5,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 70,
+    },
+    opacity: {
+      value: 0.3,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 0.5, max: 3 },
+    },
+  },
+};
+
+export { initEngine, starsOptions };
