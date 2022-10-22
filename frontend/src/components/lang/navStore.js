de991f79500f98b2f49878136faf03390488f530
Jad Yahya <jadoyahya@gmail.com>
2022-10-23 01:50:25 +0300

    [Frontend | Config]: Added multi lang support for navbar

---
+++ frontend/src/components/lang/navStore.js
@@ -0,0 +1,19 @@
+const navStore = {
+  english: {
+    docs: "Docs",
+    download: "Download",
+    contact: "Contact",
+  },
+  french: {
+    docs: "Docs",
+    download: "Télécharger",
+    contact: "Contactez",
+  },
+  chinese: {
+    docs: "文件",
+    download: "下载",
+    contact: "接触",
+  },
+};
+
+export default navStore;
