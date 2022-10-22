a1e79b8e7ca35ffb7f27df0fbc9c0c9d7964adf7
Jad Yahya <jadoyahya@gmail.com>
2022-10-23 01:40:41 +0300

    [Frontend | Components]: Changed ide info component structure

--- frontend/src/components/IdeInfo.jsx
+++ frontend/src/components/IdeInfo.jsx
@@ -1,29 +1,19 @@
-const infoStore = {
-  collab: {
-    title: "Collaboration Power",
-    body: "Bored to code alone and to explain your code for your friends without visualizing it? We got you. Code along with your friends and chat with them directly throught the editor using voice chat or messaging feature.",
-    image: require("../assets/collaboration.svg").default,
-  },
-  github: {
-    title: "Github integration",
-    body: "In addition to your online profile storage, we did integrate your favorite code hosting platform Github! in case you want to share your code there! Push and import files directly from there.",
-    image: require("../assets/github.png"),
-    classMame: "w-[270px]",
-  },
-};
+import infoStore from "./lang/infoStore";
 
 const IdeInfo = () => {
+  const lang = localStorage.getItem("lang-preference") || "english";
+
   return (
     <>
-      {Object.entries(infoStore).map(([key, value]) => {
+      {Object.values(infoStore[lang]).map((value) => {
         return (
           <div
             className='flex flex-col md:flex-row items-center justify-center  md:w-2/3 gap-10 mx-auto lg:gap-40'
-            key={key}>
+            key={value.title}>
             <img
               src={value.image}
               alt={value.title}
-              title={key}
+              title={value.title}
               className={value.classMame || ""}
             />
             <div className='flex flex-col justify-center gap-2 md:w-1/2 mx-10 text-left text-white lg:scale-125 '>
