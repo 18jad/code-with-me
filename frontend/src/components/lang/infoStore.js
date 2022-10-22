d19f893b8960f43ccbbb15ec5e413bea3638bdda
Jad Yahya <jadoyahya@gmail.com>
2022-10-23 01:39:51 +0300

    [Frontend | Config]: Added language support for ide info

---
+++ frontend/src/components/lang/infoStore.js
@@ -0,0 +1,43 @@
+const infoStore = {
+  english: {
+    collab: {
+      title: "Collaboration Power",
+      body: "Bored to code alone and to explain your code for your friends without visualizing it? We got you. Code along with your friends and chat with them directly throught the editor using voice chat or messaging feature.",
+      image: require("../../assets/collaboration.svg").default,
+    },
+    github: {
+      title: "Github integration",
+      body: "In addition to your online profile storage, we did integrate your favorite code hosting platform Github! in case you want to share your code there! Push and import files directly from there.",
+      image: require("../../assets/github.png"),
+      classMame: "w-[270px]",
+    },
+  },
+  french: {
+    collab: {
+      title: "Puissance de la collaboration",
+      body: "Vous êtes fatigué de coder seul et d'expliquer votre code à vos amis sans le visualiser? Nous avons la solution. Codez avec vos amis et discutez avec eux directement à travers l'éditeur en utilisant la fonction de chat vocal ou de messagerie.",
+      image: require("../../assets/collaboration.svg").default,
+    },
+    github: {
+      title: "Intégration Github",
+      body: "En plus de votre stockage de profil en ligne, nous avons intégré votre plateforme de codage préférée Github! au cas où vous voudriez partager votre code là-bas! Poussez et importez des fichiers directement depuis là-bas.",
+      image: require("../../assets/github.png"),
+      classMame: "w-[270px]",
+    },
+  },
+  chinese: {
+    collab: {
+      title: "协作力量",
+      body: "烦恼于单独编程和向朋友解释代码而无法可视化？我们有办法。与你的朋友一起编程，并直接在编辑器中使用语音聊天或消息功能与他们聊天。",
+      image: require("../../assets/collaboration.svg").default,
+    },
+    github: {
+      title: "Github集成",
+      body: "除了您的在线个人资料存储之外，我们还集成了您最喜欢的代码托管平台Github！如果您想在那里分享您的代码！直接从那里推送和导入文件。",
+      image: require("../../assets/github.png"),
+      classMame: "w-[270px]",
+    },
+  },
+};
+
+export default infoStore;
