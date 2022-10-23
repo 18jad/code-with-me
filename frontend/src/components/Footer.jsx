3bb9b76256b11d9f0b89607d0353e8e9d89d112b
Jad Yahya <jadoyahya@gmail.com>
2022-10-23 15:22:22 +0300

    [Frontend | Components]: Added multi lang support to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,12 @@
 import Icon from "assets/icons/icons";
 import { tw } from "utils/TailwindComponent";
+import footerStore from "./lang/footerStore";
 import Logo from "./Logo";
 
 const Footer = ({ contact }) => {
+  const lang = localStorage.getItem("lang-preference") || "english";
+  const langComp = footerStore[lang];
+
   const ListChild = tw.li`
         text-gray
         transition
@@ -17,13 +21,13 @@ const Footer = ({ contact }) => {
 
   return (
     <div
-      className='p-14 flex flex-col md:flex-row text-white items-center justify-between
-    md:px-40 bg-white/10 border-t border-white/30 backdrop-blur-sm mt-40 gap-20 md:gap-0'>
+      className='p-14 flex flex-col lg:flex-row text-white items-center justify-between
+    md:px-40 bg-white/10 border-t border-white/30 backdrop-blur-sm mt-40 gap-20 lg:gap-0'>
       <div className='firstSlice flex flex-col gap-4 w-full'>
         <Logo width={50} />
         <p className='logo_name text-xl'>Code With Me</p>
         <span className='copyright text-sm text-gray'>
-          © Copyright 2022 CodeWithMe Inc.
+          {langComp.copyright}
         </span>
         <a
           href='https://github.com/18jad/code-with-me'
@@ -35,23 +39,23 @@ const Footer = ({ contact }) => {
       </div>
       <div className='secondSlice flex flex-wrap md:flex-nowrap flex-row md:gap-40 gap-10 w-full'>
         <div className='downloads flex-grow'>
-          <span className='title'>Downloads</span>
+          <span className='title'>{langComp.download.title}</span>
           <ul>
-            <ListChild>Web App</ListChild>
+            <ListChild>{langComp.download.web}</ListChild>
             <ListChild>Android</ListChild>
             <ListChild>IOS</ListChild>
           </ul>
         </div>
         <div className='about flex-grow '>
-          <span className='title'>About</span>
+          <span className='title'>{langComp.about.title}</span>
           <ul>
-            <ListChild>Docs</ListChild>
-            <ListChild>About us</ListChild>
-            <ListChild onClick={contact}>Contact</ListChild>
+            <ListChild>{langComp.about.docs}</ListChild>
+            <ListChild>{langComp.about.us}</ListChild>
+            <ListChild onClick={contact}>{langComp.about.contact}</ListChild>
           </ul>
         </div>
         <div className='developers flex-grow'>
-          <span className='title'>Developers</span>
+          <span className='title'>{langComp.developers}</span>
           <ul>
             <ListChild>
               <a
