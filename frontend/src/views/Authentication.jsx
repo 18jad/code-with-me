a0963d6f0139b9b93553cb1aa399ee2e04db94b3
Jad Yahya <jadoyahya@gmail.com>
2022-10-23 21:09:34 +0300

    [Frontend | Views]: Added auth logo in auth view

--- frontend/src/views/Authentication.jsx
+++ frontend/src/views/Authentication.jsx
@@ -1,15 +1,22 @@
+import TextLogo from "components/TextLogo";
 import Particles from "react-tsparticles";
 import { initEngine, starsOptions } from "./particles/StarsParticles";
+import styles from "./styles/Authentication.module.scss";
 
 const Authentication = () => {
   return (
-    <div className='bg-black h-screen'>
+    <div className={styles.pageWrapper}>
+      {/* Stars Background */}
       <Particles
         options={starsOptions}
         id='tsparticles'
         init={initEngine}
         className='h-full absolute w-full pointer-events-none'
       />
+      {/* Authentication Logo */}
+      <div className={styles.authLogo}>
+        <TextLogo text='Authentication' width={65} />
+      </div>
     </div>
   );
 };
