41946949321a0507d3bbab2bdc4289c53ec20496
Jad Yahya <jadoyahya@gmail.com>
2022-10-23 15:22:07 +0300

    [Frontend | Config]: Added footer language store

---
+++ frontend/src/components/lang/footerStore.js
@@ -0,0 +1,46 @@
+const footerStore = {
+  english: {
+    copyright: "© Copyright 2022 CodeWithMe Inc.",
+    download: {
+      title: "Downloads",
+      web: "Web App",
+    },
+    about: {
+      title: "About",
+      docs: "Docs",
+      contact: "Contact",
+      us: "About us",
+    },
+    developers: "Developers",
+  },
+  french: {
+    copyright: "© Droits d'auteur 2022 CodeWithMe Inc.",
+    download: {
+      title: "Téléchargements",
+      web: "Application Web",
+    },
+    about: {
+      title: "Sur",
+      docs: "Docs",
+      contact: "Contactez",
+      us: "À propos de nous",
+    },
+    developers: "Développeurs",
+  },
+  chinese: {
+    copyright: "© 版权所有 2022 CodeWithMe Inc.",
+    download: {
+      title: "下载",
+      web: "网页应用",
+    },
+    about: {
+      title: "关于",
+      docs: "文件",
+      contact: "接触",
+      us: "关于我们",
+    },
+    developers: "开发人员",
+  },
+};
+
+export default footerStore;
