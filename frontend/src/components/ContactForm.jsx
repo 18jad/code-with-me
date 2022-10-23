5ae1038e20a10a1a77f9a6404b0ba13b16c3281b
Jad Yahya <jadoyahya@gmail.com>
2022-10-23 15:11:00 +0300

    [Frontend | Components]: Added multi lang support for contact form

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,7 +1,11 @@
 import Icon from "assets/icons/icons";
 import { tw } from "utils/TailwindComponent";
+import contactStore from "./lang/contactStore";
 
 const ContactForm = () => {
+  const lang = localStorage.getItem("lang-preference") || "english";
+  const langComp = contactStore[lang];
+
   const FormInput = tw.input`
     bg-white/10
     border
@@ -46,21 +50,22 @@ const ContactForm = () => {
 
   return (
     <div className='bg-white/10 rounded-md md:w-2/3 mx-auto flex flex-col items-center r p-10 border border-white/30 backdrop-blur-sm gap-14 w-11/12  h-full'>
-      <h1 className='text-3xl md:text-5xl text-white'>Contact Me</h1>
+      <h1 className='text-3xl md:text-5xl text-white'>{langComp.title}</h1>
       <form className='inputs flex flex-col w-full m-auto justify-center items-end md:gap-14'>
         <div className='flex flex-col md:flex-row w-full h-full gap-10 my-10 justify-center'>
           <div className='left_inputs flex flex-col gap-14 w-full md:w-1/3'>
-            <FormInput placeholder='Full name' type='text' required />
-            <FormInput placeholder='Email address' type='email' required />
-            <FormInput placeholder='Subject' type='text' required />
+            <FormInput placeholder={langComp.name} type='text' required />
+            <FormInput placeholder={langComp.email} type='email' required />
+            <FormInput placeholder={langComp.subject} type='text' required />
           </div>
           <div className='right_inputs w-full md:w-1/2 h-[200px] md:h-auto'>
-            <TextArea placeholder='Message' />
+            <TextArea placeholder={langComp.message} />
           </div>
         </div>
         <div className='flex w-full justify-center px-2 flex-row-reverse'>
           <button className='bg-white/10 border border-gray-500 px-3 py-1 rounded-sm text-white hover:bg-white/5 transition duration-150 flex items-center justify-center'>
-            Send <Icon i='sendArrow' height={15} />
+            <span>{langComp.send}</span>
+            <Icon i='sendArrow' height={15} />
           </button>
           <div className='w-4/5 hidden md:block'></div>
         </div>
