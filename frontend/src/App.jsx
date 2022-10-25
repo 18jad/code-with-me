a66eebdcc743ff58d3c195854a53f985c09e7d3e
Jad Yahya <jadoyahya@gmail.com>
2022-10-25 21:54:29 +0300

    [Frontend | App]: Updated app to use animated page transition on route change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
-import { Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { ProtectedRoute } from "utils/ProtectedRoute";
 import Authentication from "views/Authentication";
 import Home from "views/Home";
 
 function App() {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route
-        path='/auth'
-        element={
-          <ProtectedRoute
-            condition={true}
-            redirect=''
-            access={<Authentication />}
-          />
-        }></Route>
-    </Routes>
+    <AnimatePresence exitBeforeEnter>
+      {/* key is passed to help animate presence know which child is mounting */}
+      <Routes location={location} key={location.pathname}>
+        <Route path='/' element={<Home />} />
+        <Route
+          path='/auth'
+          element={
+            <ProtectedRoute
+              condition={true}
+              redirect=''
+              access={<Authentication />}
+            />
+          }></Route>
+      </Routes>
+    </AnimatePresence>
   );
 }
 
