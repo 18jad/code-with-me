3a6791fd2823841475d0a5ffab286f7ec3979288
Jad Yahya <jadoyahya@gmail.com>
2022-10-26 21:19:17 +0300

    [Frontend | Views]: Added search bar and reesult to profile page

--- frontend/src/views/Profile.jsx
+++ frontend/src/views/Profile.jsx
@@ -1,8 +1,15 @@
+import SearchUser from "components/profile/SearchUser";
 import TextLogo from "components/TextLogo";
 import { MagnifyingGlass, SignOut } from "phosphor-react";
+import { useState } from "react";
 import styles from "./styles/Profile.module.scss";
 
 const Profile = () => {
+  // TODO: use state and store searched user data inside it
+  let usersList = false;
+
+  const [searchState, setSearchState] = useState(false);
+
   return (
     <div className={styles.pageWrapper}>
       {/* Logo + sign out section*/}
@@ -30,6 +37,17 @@ const Profile = () => {
             type='text'
             placeholder='Search for a user'
             className='bg-white/5 border shadow-sm pl-10  pr-2 py-2 placeholder-gray-300 border-gray-500 focus:border-gray-800  focus:ring-2 focus:bg-black/10 focus:ring-gray-500 outline-none rounded transition duration-150 text-white w-full'
+            onChange={(e) => {
+              let searchValue = e.target.value;
+              setSearchState(Boolean(searchValue.trim().length > 0));
+            }}
+            onBlur={() => {
+              setSearchState(false);
+            }}
+            onFocus={(e) => {
+              let searchValue = e.target.value;
+              setSearchState(Boolean(searchValue.trim().length > 0));
+            }}
           />
           <MagnifyingGlass
             size={22}
@@ -37,6 +55,21 @@ const Profile = () => {
             className={styles.searchIcon}
             weight='bold'
           />
+        </div>
+        <div
+          className={`${styles.searchResultWrapper} transition duration-300 ${
+            searchState ? "opacity-1" : "opacity-0"
+          }`}>
+          {/* TODO: Change opacity based on fetched user list */}
+          {usersList ? (
+            <>
+              <SearchUser name='John Doe1' username='john_doe' />
+              <SearchUser name='John Doe' username='john_doe' />
+              <SearchUser name='John Doe' username='john_doe' />
+            </>
+          ) : (
+            <p className='text-white text-center py-2'>No results found</p>
+          )}
         </div>
       </div>
     </div>
