d052a9b8b6a664e3dc5561d2b92b8ab384792076
Jad Yahya <jadoyahya@gmail.com>
2022-10-26 16:29:59 +0300

    [Frontend | Views]: Added interactions section in profile view

--- frontend/src/views/Profile.jsx
+++ frontend/src/views/Profile.jsx
@@ -1,10 +1,24 @@
+import TextLogo from "components/TextLogo";
+import { SignOut } from "phosphor-react";
 import styles from "./styles/Profile.module.scss";
 
 const Profile = () => {
   return (
     <div className={styles.pageWrapper}>
       {/* Logo + sign out section*/}
-      <div className={styles.interactionsSection}></div>
+      <div className={styles.interactionsSection}>
+        <div className={styles.logo}>
+          <TextLogo text='Profile' width={55} mainSize='xl' textSize='sm' />
+        </div>
+        <div className={styles.interactionsButtons}>
+          <button className={styles.signOutBtn}>
+            <span>
+              <SignOut size={20} color='#fff' mirrored={true} />
+            </span>
+            <span>Logout</span>
+          </button>
+        </div>
+      </div>
 
       {/* Profile feed section */}
       <div className={styles.profileFeedSection}></div>
