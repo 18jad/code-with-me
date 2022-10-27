b5b08bc07783b74980698a6afe969408e15d2982
Jad Yahya <jadoyahya@gmail.com>
2022-10-27 23:48:55 +0300

    [Frontend | Components]: Improved modal component

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,20 +1,31 @@
 import { X } from "phosphor-react";
 
-const Modal = ({ isOpen, title, children, onClick, bg = "#333" }) => {
+const Modal = ({
+  isOpen,
+  title,
+  children,
+  onClick,
+  bg = "#333",
+  className = [],
+}) => {
   return (
     <div
-      className={`background_modal absolute bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#333]/10 via-black/30 to-black/40 w-screen h-screen ${
+      className={`background_modal fixed bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#333]/10 via-black/30 to-black/40 w-screen h-screen ${
         isOpen
           ? "opacity-1 pointer-events-auto"
           : "opacity-0 pointer-events-none"
-      } top-1/2 left-1/2 -translate-x-1/2 transition duration-200 -translate-y-1/2 grid place-content-center`}
+      } top-1/2 left-1/2 -translate-x-1/2 transition duration-200 -translate-y-1/2 grid place-content-center `}
       onClick={onClick}>
       <div
-        className={`modal bg-[${
-          bg ? bg : " bg-gray-800 "
-        }] flex flex-col items-center justify-center h-fit transition duration-500 gap-10 p-10 rounded relative ${
-          isOpen ? "opacity-1" : "opacity-0"
-        }`}
+        className={
+          `modal bg-[${
+            bg ? bg : " bg-gray-800 "
+          }] flex flex-col items-center justify-center h-fit transition duration-500 gap-10 p-10 rounded relative ${
+            isOpen ? "opacity-1" : "opacity-0"
+          }` +
+          " " +
+          [...className].join("")
+        }
         onClick={(e) => {
           e.stopPropagation();
         }}>
