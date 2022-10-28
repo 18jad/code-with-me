8381819aead0cc502e1e2df9e2461f3e7e66e5f9
Jad Yahya <jadoyahya@gmail.com>
2022-10-28 22:04:22 +0300

    [Frontend | Components]: Created and added required files for tree component

---
+++ frontend/src/components/editor/SidebarContent.jsx
@@ -0,0 +1,83 @@
+import { useLayoutEffect, useState } from "react";
+import Tree from "./FileTree/Tree";
+
+const structure = [
+  {
+    type: "folder",
+    name: "client",
+    files: [
+      {
+        type: "folder",
+        name: "ui",
+        files: [
+          { type: "file", name: "Toggle.js" },
+          { type: "file", name: "Button.js" },
+          { type: "file", name: "Button.style.js" },
+        ],
+      },
+      {
+        type: "folder",
+        name: "components",
+        files: [
+          { type: "file", name: "Tree.js" },
+          { type: "file", name: "Tree.style.js" },
+        ],
+      },
+      { type: "file", name: "setup.js" },
+      { type: "file", name: "setupTests.js" },
+    ],
+  },
+  {
+    type: "folder",
+    name: "packages",
+    files: [
+      {
+        type: "file",
+        name: "main.js",
+      },
+    ],
+  },
+  { type: "file", name: "index.js" },
+];
+
+const FileStructure = () => {
+  let [data, setData] = useState(structure);
+
+  const handleClick = (node) => {
+    console.log(node);
+  };
+  const handleUpdate = (state) => {
+    localStorage.setItem(
+      "tree",
+      JSON.stringify(state, function (key, value) {
+        if (key === "parentNode" || key === "id") {
+          return null;
+        }
+        return value;
+      }),
+    );
+  };
+
+  useLayoutEffect(() => {
+    try {
+      let savedStructure = JSON.parse(localStorage.getItem("tree"));
+      if (savedStructure) {
+        setData(savedStructure);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+  return (
+    <div className='App'>
+      <h2>Imparative API (editable)</h2>
+
+      <Tree data={data} onUpdate={handleUpdate} onNodeClick={handleClick} />
+    </div>
+  );
+};
+const SidebarContent = () => {
+  return <FileStructure />;
+};
+
+export default SidebarContent;
