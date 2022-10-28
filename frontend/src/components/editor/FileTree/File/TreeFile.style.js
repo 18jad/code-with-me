55de3906249b54a37d6f27e282c06cee1581f5de
Jad Yahya <jadoyahya@gmail.com>
2022-10-28 22:29:18 +0300

    [Frontend | Styling]: Adjusted file styling

--- frontend/src/components/editor/FileTree/File/TreeFile.style.js
+++ frontend/src/components/editor/FileTree/File/TreeFile.style.js
@@ -4,6 +4,8 @@ export const StyledFile = styled.div`
   flex-wrap: nowrap;
   display: flex;
   align-items: center;
-  font-weight: normal;
+  font-weight: 300;
+  text-shadow: 0 0 1px #fff, 0 0 1px #fff;
+  font-size: 15px;
   padding-left: ${(p) => p.theme.indent}px;
 `;
