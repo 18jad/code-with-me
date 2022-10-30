f5df33c4f0ae0da4fd5fa4ffc1d520b4dc637e76
Jad Yahya <jadoyahya@gmail.com>
2022-10-30 18:11:12 +0200

    [Frontend | Refactor]: Refactored sidebar content swapping content

--- frontend/src/components/editor/SidebarContent.jsx
+++ frontend/src/components/editor/SidebarContent.jsx
@@ -2,34 +2,22 @@ import ChatConversation from "components/ChatConversation";
 import FileStructure from "./FileStructure";
 import GithubTool from "./GithubTool";
 
+const swapContent = (content, compare) => {
+  return content === compare
+    ? "opacity-1"
+    : "opacity-0 h-0 w-0 -z-50 pointer-events-none sticky top-0 self-stretch";
+};
+
 const SidebarContent = ({ content }) => {
-  // Here we are hiding the element under the current active element
+  // Here we are hiding the element under the current active element using swapContent function
   // Instead of just rendering another component
   // This is to prevent the sidebar content from being re-rendered and losing old content/state
 
   return (
     <>
-      <FileStructure
-        className={`${
-          content === "fileTree"
-            ? "opacity-1"
-            : "opacity-0 h-0 w-0 -z-50 pointer-events-none"
-        } sticky top-0 self-stretch`}
-      />
-      <GithubTool
-        className={`${
-          content === "github"
-            ? "opacity-1"
-            : "opacity-0 h-0 w-0 -z-50 pointer-events-none"
-        } sticky top-0 self-stretch`}
-      />
-      <ChatConversation
-        className={`${
-          content === "chat"
-            ? "opacity-1"
-            : "opacity-0 h-0 w-0 -z-50 pointer-events-none"
-        }`}
-      />
+      <FileStructure className={swapContent(content, "fileTree")} />
+      <GithubTool className={swapContent(content, "github")} />
+      <ChatConversation className={swapContent(content, "chat")} />
     </>
   );
 };
