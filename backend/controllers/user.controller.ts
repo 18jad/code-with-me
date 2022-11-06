9ec317f324966a4384dc7118ddf244bb5e425e3d
Jad Yahya <jadoyahya@gmail.com>
2022-11-06 16:15:08 +0200

    [Backend | Controllers]: Created create new project method with validation

--- backend/controllers/user.controller.ts
+++ backend/controllers/user.controller.ts
@@ -121,6 +121,11 @@ class UserController {
     }
   }
 
+  /**
+   * @description Validate title length and characters
+   * @param title {string} project title
+   * @returns {boolean} true if title is valid
+   */
   public validateTitle(title: string) {
     const titleLength: number = Number(title?.length);
     return (
@@ -130,11 +135,21 @@ class UserController {
     );
   }
 
+  /**
+   * @description Validate description length
+   * @param description {string} project description
+   * @returns {boolean} true if description is valid
+   */
   public validateDescription(description: string) {
     const descriptionLength: number = Number(description?.length);
     return descriptionLength > 10 && descriptionLength < 80; // min is 10 characters and max is 100 characters
   }
 
+  /**
+   * @description Check if title already exists in database
+   * @param title {string} project title
+   * @returns {Promise<boolean>} true if title is found
+   */
   public async checkTitle(title: string) {
     const foundProject = await prisma.project.findFirst({
       where: {
@@ -144,13 +159,18 @@ class UserController {
     return Boolean(foundProject) ? true : false;
   }
 
+  /**
+   * @description Validate all project fields
+   * @param data {Request} project data
+   * @returns {Promise<boolean>} true if all project inputs are valid
+   */
   public async validate(data: Request) {
     const { title, description } = data.body as Project;
     return new Promise((resolve, reject) => {
-      if (this.validateTitle(title)) {
+      if (title && this.validateTitle(title)) {
         this.checkTitle(title).then((found: boolean) => {
           if (!found) {
-            if (this.validateDescription(description)) {
+            if (description && this.validateDescription(description)) {
               resolve({ title, description });
             } else {
               reject(
@@ -167,6 +187,50 @@ class UserController {
         );
       }
     });
+  }
+
+  /**
+   * @description Validate fields and create a new project
+   * @param request {Request}
+   * @param response {Response}
+   * @returns {void} Create the project
+   */
+  public async createProject(request: Request, response: Response) {
+    this.decodeToken(request)
+      .then((token: any) => {
+        const { id } = token;
+        this.validate(request)
+          .then((result) => {
+            const { title, description } = result as Project;
+            const project = prisma.project
+              .create({
+                data: {
+                  title: title,
+                  description: description,
+                  fileStructure: JSON.parse(
+                    `{"type":"folder","name":"${title}","path":"/projects/${title}","files":[]}`,
+                  ),
+                  author: {
+                    connect: {
+                      id: id,
+                    },
+                  },
+                },
+              })
+              .then((project) => {
+                sendResponse(response, true, "Project created", { project });
+              })
+              .catch((error) => {
+                sendResponse(response, false, "Project not created", error);
+              });
+          })
+          .catch((error) => {
+            sendResponse(response, false, error);
+          });
+      })
+      .catch((error) => {
+        sendResponse(response, false, "Unauthorized user", error);
+      });
   }
 }
 
