62caa86f77999e14cc3de990fbdf6f7ffe847664
Jad Yahya <jadoyahya@gmail.com>
2022-11-09 00:09:00 +0200

    [Backend | Controller]: Created liking and unliking functionalities

--- backend/controllers/user.controller.ts
+++ backend/controllers/user.controller.ts
@@ -341,6 +341,114 @@ class UserController {
       sendResponse(response, false, "Query is empty");
     }
   }
+
+  /**
+   * @description Check if user already liking another user
+   * @param id {number | string} logged in user id
+   * @param userId {number | string} likked user id
+   * @returns {Promise<boolean>} true if user is already liked
+   */
+  public async checkIfLiked(id: number | string, userId: number | string) {
+    if (typeof userId === "number" || typeof userId === "string") {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: Number(userId),
+        },
+        select: {
+          likes: true,
+        },
+      });
+      let likes = user?.likes as unknown as number[];
+      return { likedUsers: likes, isLiked: likes?.includes(Number(id)) };
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * @description Like or unlike another user, based on your current status
+   * @param request {Request}
+   * @param response {Response}
+   * @returns {void} Like a user
+   */
+  public async updateUserLike(request: Request, response: Response) {
+    this.decodeToken(request)
+      .then(async (token: any) => {
+        if (token) {
+          const { id } = token as { id: number };
+          const { userId } = request.body as { userId: number };
+          const { likedUsers, isLiked } = (await this.checkIfLiked(
+            id,
+            userId,
+          )) as any;
+          if (isLiked) {
+            // Already liked, so unlike
+            // Remove the logged in user id from user id likes array, and delete any duplicated if there is any
+            const uniqueLikes = Array.from(
+              new Set(
+                likedUsers.filter((uid: number) => {
+                  return uid !== id;
+                }),
+              ),
+            ) as number[];
+            prisma.user
+              .update({
+                where: {
+                  id: Number(userId),
+                },
+                data: {
+                  likes: {
+                    set: uniqueLikes,
+                  },
+                  likesCount: {
+                    decrement: 1, // decrement likes counter by 1
+                  },
+                },
+              })
+              .then((result) => {
+                sendResponse(response, true, "User unliked successfully", {
+                  result,
+                });
+              })
+              .catch((error) => {
+                sendResponse(response, false, "Something went wrong", error);
+              });
+          } else {
+            // Not liked, like the user
+            // Add the logged in user id to the user id likes array, and delete any duplicated if there is any
+            const uniqueLikes = Array.from(
+              (new Set<number[]>([...likedUsers, id]) as any).values(),
+            ) as number[];
+            console.log(uniqueLikes, id, [...likedUsers, id]);
+            prisma.user
+              .update({
+                where: {
+                  id: Number(userId),
+                },
+                data: {
+                  likes: {
+                    set: uniqueLikes,
+                  },
+                  likesCount: {
+                    increment: 1, // increase likes counter by 1
+                  },
+                },
+              })
+              .then((result) => {
+                sendResponse(response, true, "User liked successfully", {
+                  result,
+                });
+              })
+              .catch((error) => {
+                sendResponse(response, false, "Something went wrong", error);
+              });
+          }
+        }
+      })
+      .catch((error) => {
+        sendResponse(response, false, "Unauthorized user", error);
+      });
+  }
 }
 
 module.exports = UserController;
