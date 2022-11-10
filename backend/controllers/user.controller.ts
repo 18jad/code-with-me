831d4587bf73ff5ea56e577a0e0d9a3e7c16e379
Jad Yahya <jadoyahya@gmail.com>
2022-11-10 22:39:25 +0200

    [Backend | Controllers]: Moved project related methods from user controller to project controller

--- backend/controllers/user.controller.ts
+++ backend/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 // Imported packages
-import { PrismaClient, Project, User } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
 import { Request } from "express";
 const AuthUser = require("./auth.controller");
 const prisma = new PrismaClient();
@@ -9,8 +9,6 @@ const jwt = require("jsonwebtoken");
 const userCheck = new AuthUser();
 
 class UserController {
-  private readonly titleFilter = /^(?=.*[a-zA-Z]+.*)[A-Za-z0-9_-]{4,20}$/;
-
   /**
    * @description Extract jwt token from request header
    * @param request {Request}
@@ -47,30 +45,6 @@ class UserController {
   }
 
   /**
-   * @description Validate title length and characters
-   * @param title {string} project title
-   * @returns {boolean} true if title is valid
-   */
-  public validateTitle(title: string) {
-    const titleLength: number = Number(title?.length);
-    return (
-      titleLength >= 3 &&
-      titleLength <= 20 && // min is 3 characters and max is 20 characters and it should be unique
-      this.titleFilter.test(title) // it should contain only letters, numbers, underscores and hyphens
-    );
-  }
-
-  /**
-   * @description Validate description length
-   * @param description {string} project description
-   * @returns {boolean} true if description is valid
-   */
-  public validateDescription(description: string) {
-    const descriptionLength: number = Number(description?.length);
-    return descriptionLength > 10 && descriptionLength < 80; // min is 10 characters and max is 80 characters
-  }
-
-  /**
    * @description Validate headline length
    * @param headline {string} user headline
    * @returns {boolean} true if headline is valid
@@ -78,94 +52,6 @@ class UserController {
   public validateHeadline(headline: string) {
     const headlineLength: number = Number(headline?.length);
     return headlineLength > 10 && headlineLength < 100; // min is 10 characters and max is 100 characters
-  }
-
-  /**
-   * @description Check if title already exists in database
-   * @param title {string} project title
-   * @returns {Promise<boolean>} true if title is found
-   */
-  public async checkTitle(title: string) {
-    const foundProject = await prisma.project.findFirst({
-      where: {
-        title: title,
-      },
-    });
-    return Boolean(foundProject) ? true : false;
-  }
-
-  /**
-   * @description Validate all project fields
-   * @param data {Request} project data
-   * @returns {Promise<boolean>} true if all project inputs are valid
-   */
-  public async validate(data: Request) {
-    const { title, description } = data.body as Project;
-    return new Promise((resolve, reject) => {
-      if (title && this.validateTitle(title)) {
-        this.checkTitle(title).then((found: boolean) => {
-          if (!found) {
-            if (description && this.validateDescription(description)) {
-              resolve({ title, description });
-            } else {
-              reject(
-                "Invalid description. Min is 10 characters and max is 80 characters",
-              );
-            }
-          } else {
-            reject("Title already exists");
-          }
-        });
-      } else {
-        reject(
-          "Invalid title. Min is 4 characters and max is 20 characters, it should contain only letters, numbers, underscores (_) and hyphens (-)",
-        );
-      }
-    });
-  }
-
-  /**
-   * @description Validate fields and create a new project
-   * @param request {Request}
-   * @param response {Response}
-   * @returns {void} Create the project
-   */
-  public async createProject(request: Request, response: Response) {
-    this.decodeToken(request)
-      .then((token: any) => {
-        const { id } = token;
-        this.validate(request)
-          .then((result) => {
-            const { title, description } = result as Project;
-            const project = prisma.project
-              .create({
-                data: {
-                  title: title,
-                  description: description,
-                  fileStructure: JSON.parse(
-                    `{"type":"folder","name":"${title}","path":"/projects/${title}","files":[]}`,
-                  ),
-                  author: {
-                    connect: {
-                      id: id,
-                    },
-                  },
-                },
-              })
-              .then((project) => {
-                sendResponse(response, true, "Project created", { project });
-              })
-              .catch((error) => {
-                sendResponse(response, false, "Project not created", error);
-              });
-          })
-          .catch((error) => {
-            sendResponse(response, false, error);
-          });
-      })
-      .catch((error) => {
-        sendResponse(response, false, "Unauthorized user", error);
-      });
   }
 
   /**
@@ -463,6 +349,9 @@ class UserController {
         sendResponse(response, false, "Unauthorized user", error);
       });
   }
+
+  // create a seperate file for project page method and controller
+  // create send invitation by email
 }
 
 module.exports = UserController;
