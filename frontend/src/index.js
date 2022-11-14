ef381c65c9ca9fe6f4fa36e702a02d313a4f57af
Jad Yahya <jadoyahya@gmail.com>
2022-11-14 15:53:24 +0200

    [Frontend | App]: Included query provider and dev tools in app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "App";
 import "index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <React.Suspense
       fallback={<div className='text-white text-2xl'>Loading...</div>}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </React.Suspense>
   </BrowserRouter>,
 );
