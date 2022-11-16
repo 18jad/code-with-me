88204fe90edbf27c8103da5faa68a894cec24694
Jad Yahya <jadoyahya@gmail.com>
2022-11-17 01:14:06 +0200

    [Frontend | Utils]: Added axios interceptors to axios instance

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -7,17 +7,17 @@ const PORT = 2121;
 // Your current api version
 const API_VERSION = "1.0";
 
+// Your current api slug
 const SLUG = "api";
 
+// Base url of backend server
 const BASE_URL = `http://localhost:${PORT}/${SLUG}/V${API_VERSION}`;
 
-const authCookies = Cookies.get("persist:user"),
-  { user } = (authCookies && JSON.parse(authCookies)) || {
-    user: null,
-  };
+// Auth credentials (token, userinfo)
+const authCookies = Cookies.get("persist:user");
 
 // JWT Token
-const token = JSON.parse(user).authToken;
+const token = authCookies ? JSON.parse(authCookies)?.token.split('"')[1] : null;
 
 // Axios instance configurations
 const config = {
@@ -29,6 +29,7 @@ const config = {
     "Access-Control-Allow-Headers":
       "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin",
   },
+  timeout: 10000,
 };
 
 // Without authentication
@@ -36,4 +37,22 @@ export const axiosInstance = axios.create(config);
 
 // With authentication
 Object.assign(config.headers, { Authorization: `Bearer ${token}` });
+
 export const axiosUser = axios.create(config);
+
+// Axios interceptor, to handle bearer token change and expiration
+axiosUser.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("persist:user")
+      ? JSON.parse(Cookies.get("persist:user"))?.token.split('"')[1]
+      : null;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    } else {
+      delete axiosUser.defaults.headers.common.Authorization;
+    }
+    return config;
+  },
+
+  (error) => Promise.reject(error),
+);
