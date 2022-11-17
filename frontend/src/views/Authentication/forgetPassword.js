31fe06d5665656f2e06b026db304349702eda517
Jad Yahya <jadoyahya@gmail.com>
2022-11-17 05:17:55 +0200

    [Frontend | Authentication]: Created reset password functionality

--- frontend/src/views/Authentication/forgetPassword.js
+++ frontend/src/views/Authentication/forgetPassword.js
@@ -1,9 +1,11 @@
 import qs from "qs";
 import { axiosInstance } from "utils/axiosInstance";
+import Register from "./register";
 
 class ForgetController {
   #forget_url = "/auth/generate_reset";
   #validate_url = "/auth/verify_token";
+  #reset_url = "/auth/reset_password";
   // eslint-disable-next-line no-useless-escape
   #emailFilter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
@@ -60,6 +62,33 @@ class ForgetController {
         .catch((error) => {
           reject(error);
         });
+    });
+  };
+
+  handleReset = (e, token) => {
+    e.preventDefault();
+    return new Promise((resolve, reject) => {
+      const { password } = e.target.elements;
+      const validatePassowrd = new Register().validatePassword(password.value);
+      if (validatePassowrd) {
+        axiosInstance
+          .post(
+            this.#reset_url,
+            qs.stringify({ password: password.value, token }),
+          )
+          .then((response) => {
+            if (response.status === 200 && response.data.success) {
+              resolve(response.data);
+            } else {
+              reject(response.message);
+            }
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      } else {
+        reject("Password must be at least 8 characters");
+      }
     });
   };
 }
