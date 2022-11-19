56073c3969ce3964293b903b71a89a73f569748a
Jad Yahya <jadoyahya@gmail.com>
2022-11-19 20:30:15 +0200

    [Frontend | Controller]: Created share project method for project controller

--- frontend/src/views/Editor/editorController.js
+++ frontend/src/views/Editor/editorController.js
@@ -1,7 +1,10 @@
 const { axiosUser } = require("utils/axiosInstance");
 
 export class EditorController {
+  // eslint-disable-next-line no-useless-escape
+  #emailFilter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   #check_url = "/project/check_allowed";
+  #share_url = "/project/send_invitation";
 
   checkIfAllowed(project_id) {
     return new Promise((resolve, reject) => {
@@ -13,6 +16,33 @@ export class EditorController {
         .catch((err) => {
           reject(err);
         });
+    });
+  }
+
+  shareProject(e) {
+    e.preventDefault();
+    const { link, email } = e.target.elements;
+    console.log(link.value, email.value);
+    return new Promise((resolve, reject) => {
+      if (!this.#emailFilter.test(email.value)) {
+        reject("Please enter a valid email");
+      } else {
+        if (link.value === "") {
+          reject("Please enter a valid link");
+        } else {
+          axiosUser
+            .post(this.#share_url, {
+              link: link.value,
+              email: email.value,
+            })
+            .then((result) => {
+              resolve(result.data);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        }
+      }
     });
   }
 }
