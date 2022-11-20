5778856b4ccaaa60c1c46e9ce3108ecdcd7f0acb
Jad Yahya <jadoyahya@gmail.com>
2022-11-20 14:50:11 +0200

    [Frontend | Controller]: Created get access token and push code to github methods

--- frontend/src/views/Editor/editorController.js
+++ frontend/src/views/Editor/editorController.js
@@ -1,4 +1,4 @@
-const { axiosUser } = require("utils/axiosInstance");
+const { axiosUser, axiosInstance } = require("utils/axiosInstance");
 
 export class EditorController {
   // eslint-disable-next-line no-useless-escape
@@ -6,6 +6,12 @@ export class EditorController {
   #check_url = "/project/check_allowed";
   #share_url = "/project/send_invitation";
   #allowd_url = "/project/allow_user";
+  #github_access = "/github/get_access_token";
+  #push_url = "/github/push";
+
+  constructor(octokit) {
+    this.octokit = octokit;
+  }
 
   checkIfAllowed(project_id) {
     return new Promise((resolve, reject) => {
@@ -58,6 +64,51 @@ export class EditorController {
         })
         .catch((err) => {
           reject(err);
+        });
+    });
+  }
+
+  githubAccessToken(code) {
+    return new Promise((resolve, reject) => {
+      axiosInstance
+        .get(`${this.#github_access}?code=${code}`)
+        .then((result) => {
+          resolve(result.data);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  githubUserData(token) {
+    return new Promise((resolve, reject) => {
+      this.octokit.rest.users
+        .getAuthenticated()
+        .then((res) => resolve(res.data))
+        .catch((err) => reject(err));
+    });
+  }
+
+  pushCodeToGithub(e, projectTitle, authUser, access_token) {
+    e.preventDefault();
+    const { commit_message, repository_name } = e.target.elements;
+    const { email: owner_email, login: owner_username } = authUser;
+    return new Promise((resolve, reject) => {
+      axiosInstance
+        .post(this.#push_url, {
+          access_token,
+          repository_name: repository_name.value,
+          commit_message: commit_message.value,
+          project_title: projectTitle,
+          owner_email,
+          owner_username,
+        })
+        .then((result) => {
+          resolve(result.data);
+        })
+        .catch((error) => {
+          reject(error);
         });
     });
   }
