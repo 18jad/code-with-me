3625f24c4a38b5291a9be24da7aad708d955529a
Jad Yahya <jadoyahya@gmail.com>
2022-11-23 15:05:10 +0200

    [Frontend | Lang]: Added new variable in navbar language store

--- frontend/src/components/lang/navStore.js
+++ frontend/src/components/lang/navStore.js
@@ -3,16 +3,19 @@ const navStore = {
     docs: "Docs",
     goapp: "App",
     contact: "Contact",
+    register: "Register",
   },
   french: {
     docs: "Docs",
     goapp: "App",
     contact: "Contactez",
+    register: "S'inscrire",
   },
   chinese: {
     docs: "文件",
     goapp: "应用",
     contact: "接触",
+    register: "注册",
   },
 };
 
